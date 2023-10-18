b7e0114d516980dcf8cc28efb8de5117713c82a3
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-10-18 16:12:10 +0200

    License update. Releasing new version

--- src/main/java/org/burningwave/Classes.java
+++ src/main/java/org/burningwave/Classes.java
@@ -1,15 +1,15 @@
 /*
- * This file is part of Burningwave Reflection.
+ * This file is part of Burningwave Common.
  *
  * Author: Roberto Gentili
  *
- * Hosted at: https://github.com/burningwave/reflection
+ * Hosted at: https://github.com/burningwave/common
  *
  * --
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2022 Roberto Gentili
+ * Copyright (c) 2023 Roberto Gentili
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without
